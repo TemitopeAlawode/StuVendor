70bc8ff67dc39438e428620a569f677c0b5fa4af
Temitope1606 <alawodetemitope9@gmail.com>
2026-01-26 22:52:48 -0800

    updated config file

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -26,14 +26,26 @@ const config = {
     port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
     dialect: 'postgres'
   },
-  production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
-    dialect: 'postgres'
-  }
+  // production: {
+  //   username: process.env.DB_USER,
+  //   password: process.env.DB_PASSWORD,
+  //   database: process.env.DB_NAME,
+  //   host: process.env.DB_HOST,
+  //   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : undefined,
+  //   dialect: 'postgres'
+  // }
+
+   production: {
+         use_env_variable: 'DATABASE_URL', // Use single connection string
+         dialect: 'postgres',
+         dialectOptions: {
+           ssl: {
+             require: true,
+             rejectUnauthorized: false, // Allow self-signed certs (common in cloud DBs)
+           },
+         },
+         logging: false,
+       },
 }
 
 module.exports = config;
